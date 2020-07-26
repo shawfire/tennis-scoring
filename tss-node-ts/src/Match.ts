2aed4abfbab5bf95cbeff2730d4ec08389599225
John Shaw <shaw_fire@yahoo.com>
2020-07-26 12:52:54 +1000

    Can now score the game to 30-30.

--- tss-node-ts/src/Match.ts
+++ tss-node-ts/src/Match.ts
@@ -1,8 +1,3 @@
-interface Player {
-  points: number;
-  games: number;
-}
-
 enum PLAYER {
   ONE,
   TWO,
@@ -13,14 +8,27 @@ export class Match {
   player2: string;
   games: number[] = [0, 0];
   points: number[] = [0, 0];
-  lookup: string[];
+  playerLookup: Map<string, PLAYER> = new Map();
 
   constructor(player1: string, player2: string) {
     this.player1 = player1;
     this.player2 = player2;
+    this.playerLookup.set(player1, PLAYER.ONE);
+    this.playerLookup.set(player2, PLAYER.TWO);
   }
 
   score(): string {
-    return this.games[PLAYER.ONE] + "-" + this.games[PLAYER.TWO];
+    let score = this.games[PLAYER.ONE] + "-" + this.games[PLAYER.TWO];
+    if (this.points[PLAYER.ONE] == 0 && this.points[PLAYER.TWO] == 0) {
+      return score;
+    }
+    return (
+      score + ", " + this.points[PLAYER.ONE] + "-" + this.points[PLAYER.TWO]
+    );
+  }
+
+  pointWonBy(player: string) {
+    const playerIndex = this.playerLookup.get(player);
+    this.points[playerIndex] += 15;
   }
 }
--- tss-node-ts/src/tests/Match.test.ts
+++ tss-node-ts/src/tests/Match.test.ts
@@ -8,5 +8,15 @@ describe("test for class Match", () => {
 
     const match = new Match(player1, player2);
     expect(match.score()).to.equal("0-0");
+    match.pointWonBy(player1);
+    expect(match.score()).to.equal("0-0, 15-0");
+    match.pointWonBy(player2);
+    expect(match.score()).to.equal("0-0, 15-15");
+    match.pointWonBy(player1);
+    expect(match.score()).to.equal("0-0, 30-15");
+
+    //TODO implement logic to pass this test below:
+    // match.pointWonBy(player1);
+    // expect(match.score()).to.equal("0-0, 40-15");
   });
 });
